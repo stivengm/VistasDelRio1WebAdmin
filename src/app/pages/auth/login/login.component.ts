f7b194e1d3ed82cc2eafa403c5fdf11c9d868384
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-03-08 19:34:02 -0500

    ADD - Se añade loginValidators.

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { validEmail } from '../../../shared/validators/input-validators';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+
+    this.loginForm = this.fb.group({
+      email: ['',
+        [
+          Validators.required,
+          validEmail.bind(this)
+        ]
+      ],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)
+        ]
+      ]
+    });
+  }
+
+  login(): void {
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const formValue = this.loginForm.value;
+
+    console.log('Login data:', formValue);
+  }
 
 }
