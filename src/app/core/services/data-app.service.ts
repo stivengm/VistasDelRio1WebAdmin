7de215480554a39ed220b4bcc5e55138bd431c04
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-03-09 20:28:55 -0500

    ADD - Se añade algunos iconos del menu

--- src/app/core/services/data-app.service.ts
+++ src/app/core/services/data-app.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpAppService } from './http.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataAppService {
+
+    //#region evento namePage para pintar el nombre de la página actual en el header
+    private $user = new BehaviorSubject<string>("");
+
+    public setIsUser(val: string): void {
+        this.$user.next(val);
+    }
+
+    public getIsUser(): Observable<any> {
+        return this.$user.asObservable();
+    }
+
+    public getCurrentIsUser(): string {
+        return this.$user.getValue();
+    }
+    //#endregion
+
 
     //#region evento namePage para pintar el nombre de la página actual en el header
     private $namePage = new BehaviorSubject<string>("");
