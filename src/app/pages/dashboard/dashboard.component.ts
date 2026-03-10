780c0067203b77aefa3681845f3ed6fd35fd97e3
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-03-09 19:56:40 -0500

    FIX - Ajuste de animación eventos.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventsService } from '../../core/services/events.service';
 import { EventsResponse } from '../../core/models/response/events-response.model';
+import { DataAppService } from '../../core/services/data-app.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -12,10 +13,17 @@ export class DashboardComponent implements OnInit {
   allEvents: EventsResponse[] | undefined;
 
   constructor(
-    private eventsService: EventsService
+    private eventsService: EventsService,
+    private dataAppService: DataAppService
   ) {}
 
   ngOnInit(): void {
+
+    this.dataAppService.getIsUser().subscribe((user) => {
+      // TODO: Se tiene que validar la cuenta y validar si tiene acceso a añadir evento
+
+    });
+
     this.eventsService.getAllEvents().subscribe((events) => {
 
       this.allEvents = events.data;
